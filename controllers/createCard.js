f260b71ce967f26e55b6508949d6992070c32995
Prasad M <prasadmatha.504@gmail.com>
2023-02-22 12:07:20 +0530

    changes

--- controllers/createCard.js
+++ controllers/createCard.js
@@ -9,55 +9,43 @@ const saltrounds = parseInt(process.env.saltrounds);
 export default async (req, res) => {
   let body = req.body; //request body
   let mandatoryFieldsErrors = checkMandatoryFields(body, "card"); //check mandatory fields
+  let duplicateFieldsErrors = [];
   if (!mandatoryFieldsErrors.length) {
     //if no mandatory fields errors
     let users = await getData("user"); //get users data
-    //check if the user found in database or not
-    let isUserExists = users.filter(
+    //check if the user found in database or not with the given emailID in the request body
+    let userExists = users.filter(
       (user) => user.email == body.email.toLowerCase()
     );
-    if (isUserExists.length) {
+    if (userExists.length) {
       //if user found
-      let userID = isUserExists[0].id; //user_id of user
+      let userID = userExists[0].id; //user_id of user
       const cards = await getData("card"); //get cards data
       //let encryptedCardNumber = await encryptData(body.cardNumber, saltrounds);
       let id = cards.length ? cards[cards.length - 1].id + 1 : 1; //
       body.id = id;
       body.userID = userID;
       if (cards.length) {
-        //if cards data exists in database then check for duplicate fields like cardNumber etc....
-        const duplicateFieldsErrors = checkForDuplicateFields(
-          body,
-          cards,
-          "card"
-        );
-        if (!duplicateFieldsErrors.length) {
-          //if no duplicate fields errors then create row in card table and send successful response to client
-          //body.cardNumber = encryptedCardNumber;
-          let result = await createRowInTable(body, "card");
-          res.status(200).send({
-            isSuccessful: true,
-            message: `Card is saved successfully with the id :: ${id}`,
-          });
-        } else {
-          //if duplicate fields errors exists then send response to client indicating that duplicate fields.
-          res.status(400).send({
-            isSuccessful: false,
-            message: `${duplicateFieldsErrors.join(", ")} already exists`,
-          });
-        }
-      } else {
-        //if cards data not exists in database then create a row in card table directly with the data in request.
-        //body.cardNumber = encryptedCardNumber;
-        let result = await createRowInTable(body, "card"); //creating a row in card table
+        //if cards data exists then check for duplicate fields errors
+        duplicateFieldsErrors = checkForDuplicateFields(body, cards, "card");
+      }
+      if (!duplicateFieldsErrors.length) {
+        //if no duplicate fields errors
+        let result = await createRowInTable(body, "card"); //create a row in card table
         //sending successful response to client
         res.status(200).send({
           isSuccessful: true,
           message: `Card is saved successfully with the id :: ${id}`,
         });
+      } else {
+        //if duplicate fields errors then sending response to client indicating bad request
+        res.status(400).send({
+          isSuccessful: true,
+          message: `${duplicateFieldsErrors.join(", ")} already exists`,
+        });
       }
     } else {
-      //if no user found with the given emailID in the body
+      //if no user found with the given emailID in the body then sending response to client indicating user not found (404)
       res.status(404).send({
         isSuccessful: false,
         message: `No user exists with the email id :: ${body.email}`,
