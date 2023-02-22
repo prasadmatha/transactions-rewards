d665ca5695d984bb39bb7bf9f02178120f796872
Prasad M <prasadmatha.504@gmail.com>
2023-02-22 11:58:33 +0530

    changes

--- controllers/createCustomer.js
+++ controllers/createCustomer.js
@@ -9,6 +9,7 @@ const saltrounds = parseInt(process.env.saltrounds);
 export default async (req, res) => {
   let body = req.body; //request body
   let mandatoryFieldsErrors = checkMandatoryFields(body, "user"); //check mandatory fields
+  let duplicateFieldsErrors = [];
   if (!mandatoryFieldsErrors.length) {
     //if no mandatory fields errors
     const users = await getData("user"); //get users data
@@ -17,34 +18,19 @@ export default async (req, res) => {
     let id = users.length ? users[users.length - 1].id + 1 : 1;
     body.id = id;
     if (users.length) {
-      //if users data exists then check for duplicate fields
-      const duplicateFieldsErrors = checkForDuplicateFields(
-        body,
-        users,
-        "user"
-      );
-
-      if (!duplicateFieldsErrors.length) {
-        //if no duplicate fields errors
-        let result = await createRowInTable(body, "user"); //create a row in user table
-        //sending response to client
-        res.status(200).send({
-          isSuccessful: true,
-          message: `User is created successfully with the id :: ${id}`,
-        });
-      } else {
-        //sending response to client indicating duplicate fields
-        res.status(400).send({
-          isSuccessful: false,
-          message: `${duplicateFieldsErrors.join(", ")} already exists`,
-        });
-      }
-    } else {
+      duplicateFieldsErrors = checkForDuplicateFields(body, users, "user");
+    }
+    if (!duplicateFieldsErrors.length) {
       let result = await createRowInTable(body, "user"); //create a row in user table
-      //sending successful response to client
+      //sending response to client
       res.status(200).send({
         isSuccessful: true,
         message: `User is created successfully with the id :: ${id}`,
+      });
+    } else {
+      res.status(400).send({
+        isSuccessful: true,
+        message: `${duplicateFieldsErrors.join(", ")} already exists`,
       });
     }
   } else {
