6f65d1c43b0984dc21b95af08799627ba3e89ac7
Prasad M <prasadmatha.504@gmail.com>
2023-02-24 15:09:15 +0530

    changes

--- controllers/createTransaction.js
+++ controllers/createTransaction.js
@@ -8,32 +8,32 @@ import {
 
 export default async (req, res) => {
   let body = req.body;
-  let result = await checkEmailAndCardNumberValid(body); //check email and card number are valid or not?
+  let userAndCardObject = await checkEmailAndCardNumberValid(body); //check email and card number are valid or not?
   let mandatoryFieldsErrors = checkMandatoryFields(body, "transaction"); //check for mandatory fields errors
   if (!mandatoryFieldsErrors.length) {
     //if no mandatory fields errors
-    if (result != undefined) {
+    if (userAndCardObject != undefined) {
       //if email and card number are valid
-      result.trans_amount = body.transAmount;
-      let cashbackrule = await getRewardRule(result); //get reward rule based on card type and transaction amount
+      userAndCardObject.trans_amount = body.transAmount;
+      let rewardRule = await getRewardRule(userAndCardObject); //get reward rule based on card type and transaction amount
       let trans_history_data = await getData("trans_history"); //get trans_history data
       let trans_id = trans_history_data.length
         ? trans_history_data[trans_history_data.length - 1].id + 1
         : 1; //id of a new transaction
-      result.trans_id = trans_id;
-      if (cashbackrule != undefined) {
+      userAndCardObject.trans_id = trans_id;
+      if (rewardRule != undefined) {
         //if reward rule found
-        cashbackrule.trans_amount = body.transAmount;
-        let cashback = processCashback(cashbackrule, "trans_history"); //calculate cashback
-        result.cashback = cashback;
+        rewardRule.trans_amount = body.transAmount;
+        let cashback = processCashback(rewardRule, "trans_history"); //calculate cashback
+        userAndCardObject.cashback = cashback;
       } else {
-        result.cashback = 0; //if reward rule not found
+        userAndCardObject.cashback = 0; //if reward rule not found
       }
-      let response = await createRowInTable(result, "trans_history"); //create a row in trans_history table
+      let response = await createRowInTable(userAndCardObject, "trans_history"); //create a row in trans_history table
       //sending successful response to client
       res.status(200).send({
         isSuccessful: true,
-        message: `Transaction is successful with cashback :: ${result.cashback}`,
+        message: `Transaction is successful with cashback :: ${userAndCardObject.cashback}`,
       });
     } else {
       //sending response to client indicating invalid email or card number
